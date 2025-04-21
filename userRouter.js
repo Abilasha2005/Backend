6ec54420685f66c881ffca35c8dbf2f510dbf5f4
Abilasha Manne <Abilashamanne@gmail.com>
2025-04-21 10:41:33 +0530

    done

--- userRouter.js
+++ userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = express.Router;
+const routes = express.Router();
+
+
 
 const users =[
     {username:"alice",age:25,email:"alice@example.com"},
@@ -7,14 +9,14 @@ const users =[
     {username:"charlie",age:28,email:"charlie@example.com"},
 ];
 
-app.get('./users',(req,res)=>{
+routes.get('./users',(req,res)=>{
     res.status(200).json({
         messeage:"Fetched all data",
         user:users
     })
 });
 
-app.post('/users', (req, res) => {
+routes.post('/users', (req, res) => {
     const { id, username, email } = req.body;
     const newUser = { id, username, email };
     users.push(newUser);
@@ -22,6 +24,35 @@ app.post('/users', (req, res) => {
     res.status(201).json({
       message: 'User created successfully',
       user: newUser
+    });
+  });
+
+
+  routes.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { username, email } = req.body;
+    let user = users.find(user => user.id === id);
+    
+    if (user) {
+      user.username = username;
+      user.email = email;
+      res.status(200).json({
+        message: 'User updated successfully',
+        user: user
+      });
+    } else {
+      res.status(404).json({
+        message: 'User not found'
+      });
+    }
+  });
+  
+  routes.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter(user => user.id !== id);
+    
+    res.status(200).json({
+      message: 'User deleted successfully'
     });
   });
 
